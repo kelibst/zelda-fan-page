baa2c4c71674dc43dad6fcd6adc05b2d81b1d3a4
kelibst <kbooster17@gmail.com>
2020-06-24 11:47:07 +0000

    added region to s3 config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,6 @@ if Rails.env.production?
     :aws_access_key_id => ENV['S3_ACCESS_KEY'],
     
     :aws_secret_access_key => ENV['S3_SECRET_KEY'],
-    :region                 => 'eu-west-2'
     
     }
     
